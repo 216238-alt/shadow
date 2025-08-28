7320020ada350bbe64858cabdb5e53f7df897bf5
216238-alt <216238@pp.balbharati.org>
2025-08-28 09:03:27 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showArrow(ArrowNames.North)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("Tangiro")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showArrow(ArrowNames.South)
+})
+basic.forever(function () {
+	
+})
